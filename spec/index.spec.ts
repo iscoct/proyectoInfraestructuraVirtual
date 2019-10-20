584c4bf48f58ffa9c563117bdfad0d9600f67e05
iscoct <frandkv27@gmail.com>
2019-10-20 17:13:10 +0200

    El servidor puede arrancarse en cualquier puerto close #13

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -10,17 +10,34 @@ jest.mock('express', () => {
     };
 });
 
-import '../src/index';
 import express from 'express';
 
 describe('Server', () => {
     const server = express();
 
+    beforeEach(() => {
+        jest.resetModules();
+        jest.resetAllMocks();
+    });
+
     it('must listen at 8080 port', () => {
+        require('../src/index');
+
         expect(server.listen).toBeCalledWith(8080, expect.any(Function));
     });
 
+    it('must listen at the port it is assigned in the port ENV variable', () => {
+        const port = '4000';
+        process.env.PORT = port;
+
+        require('../src/index');
+
+        expect(server.listen).toHaveBeenCalledWith(port, expect.any(Function));
+    });
+
     it('must server at / and /health to check that the server is running', () => {
+        require('../src/index');
+
         expect(server.get).toHaveBeenCalledWith(['/', '/health'], expect.any(Function));
     });
 });
